1122ea20bbd403881bb1624ab22b0433be0f7c24
Jack Grahl <jack.grahl@gmail.com>
2021-02-21 00:41:30 +0000

    Implement more helpers.

--- A002402/A002405.py
+++ A002402/A002405.py
@@ -19,6 +19,54 @@ def L(n):
     return (n+1) * p // gcd(n+1, p)
 
 
+def chi(p, j):
+    """
+        >>> chi(1, 1)
+        -1
+        >>> chi(1, 3)
+        -7
+        >>> chi(2, 4)
+        11
+    """
+    return (-1)**p * A008949(p, j)
+
+
+def A008949(p, j):
+    """
+        >>> A008949(5, 5)
+        1
+        >>> A008949(0, 4)
+        16
+        >>> A008949(3, 6)
+        42
+    """
+    if p == j:
+        return 1
+    if p == 0:
+        return 2**j
+    return A008949(p-1, j-1) + A008949(p, j-1)
+
+
+def Alef(n):
+    """
+        This is A002405.
+        >>> Alef(1)
+        1
+        >>> Alef(2)
+        5
+        >>> Alef(3)
+        27
+        >>> Alef(6)
+        95435
+        >>> Alef(9)
+        262426878
+    """
+    a = P(1)
+    for i in range(0, n):
+        a = a * P(1, i)
+    x = a.integral(0, 1) * L(n)
+    return int(x + 0.5)
+
 
 def Alef_star(n):
     """
